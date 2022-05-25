b144d591d528fccf9a8c1e9edc5119332e5b6c9b
Chandan <chandanp25@gmail.com>
2022-05-25 18:44:58 +0530

    Todo Endpoint
    Added endpoints for Todo and schemas required for pydantic validation

--- api/domain/schemas/todo.py
+++ api/domain/schemas/todo.py
@@ -1,18 +1,21 @@
+from typing import Optional
+
 from api.domain.schemas.schema_base import SchemaBase
 
 
 class TodoBase(SchemaBase):
-    """your code goes here"""
-
+    description: str
 
 class TodoCreate(TodoBase):
-    pass
+    user_id: int
 
 
 class Todo(TodoBase):
+    id: Optional[int] = None
+    user_id: int
+    
     class Config:
         orm_mode = True
-
 
 class TodoResponse(Todo):
     pass
--- api/host/endpoints/todo_endpoints.py
+++ api/host/endpoints/todo_endpoints.py
@@ -1,12 +1,29 @@
+from dependency_injector.wiring import Provide, inject
+from fastapi import Depends
 from fastapi_utils.cbv import cbv
 from fastapi_utils.inferring_router import InferringRouter
+from starlette import status
 
-from api.domain.schemas.todo import TodoCreate, Todo
+from api.domain.application_container import ApplicationContainer
+from api.domain.schemas.todo import TodoCreate, Todo, TodoResponse
 from api.host.endpoints.endpoints_base import EndpointsBase
+from api.host.handler.todo_handler import TodoHandler
 
 router = InferringRouter()
 
 
 @cbv(router)
 class TodoEndpoints(EndpointsBase[TodoCreate, Todo]):
-    """Your code goes here"""
+    @inject
+    def __init__(self,  handler: TodoHandler = Depends(Provide[ApplicationContainer.handler_container.todo_handler])):
+        super().__init__(handler=handler)
+
+    @router.post('/todos', status_code=status.HTTP_201_CREATED)
+    def create(self, dto: TodoCreate) -> TodoResponse:
+        result = super().create(dto)
+        return result
+
+    @router.get('/todos/{todo_id}')
+    def get_by_id(self, todo_id: int) -> TodoResponse:
+        result = super().get_by_id(todo_id)
+        return result
